4b0f14b64b1cd25a972155191afe71169a5650cb
Henry Hoyos <hdosurrego@gmail.com>
2019-11-02 16:52:24 -0400

    changes in timeline.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Timeline from "./components/timeline";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React at Henry's Branch
-				</a>
-			</header>
+			<Timeline></Timeline>
+			<h1>Hola fuera de Timeline</h1>
 		</div>
 	);
 }
