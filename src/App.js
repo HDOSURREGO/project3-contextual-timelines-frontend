253f9714432b75066cb7bdc1648becc533053f1e
Ahernandezl0889 <ahernandezl0889@gmail.com>
2019-11-03 12:06:01 -0500

    SigUp Form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 // import logo from "./logo.svg";
 // import "./App.css";
 import LogIn from './components/LogIn'
+import SignUp from "./components/SignUp";
 
 function App() {
 	return (
 		<div className="App">
-			<LogIn/>
+			{/* <LogIn/> */}
+			<SignUp/>
 		</div>
 	);
 }
