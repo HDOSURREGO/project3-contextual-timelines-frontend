ebe4432f8dfb381d50368a4fe7d05ca5fc2aa466
Ahernandezl0889 <ahernandezl0889@gmail.com>
2019-11-04 21:13:44 -0500

    NavBar created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 // import logo from "./logo.svg";
 // import "./App.css";
-import LogIn from './components/LogIn'
+import LogIn from './components/LogIn';
 import SignUp from "./components/SignUp";
+import NavBar from './components/NavBar';
 
 function App() {
 	return (
 		<div className="App">
+			<NavBar/>
 			{/* <LogIn/> */}
 			<SignUp/>
 		</div>
