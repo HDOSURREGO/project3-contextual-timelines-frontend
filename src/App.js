637bf50cbb65442cbbcd75ca73ad10ae7a60e426
Henry Hoyos <hdosurrego@gmail.com>
2019-11-05 23:31:26 -0500

    Signup Frontend Ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import axios from "axios";
+import { Switch, Route } from "react-router-dom";
 import "../src/components/LogIn.css";
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
 import Timeline from "./components/Timeline";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			currentUser: null
+		};
 	}
 
+	componentDidMount() {
+		axios
+			.get("http://localhost:3000/checkuser", { withCredentials: true })
+			.then(responseFromTheBackend => {
+				// console.log("User in APP.JS: ", responseFromTheBackend)
+				const { userDoc } = responseFromTheBackend.data;
+				this.syncCurrentUSer(userDoc);
+			})
+			.catch(err =>
+				console.log(
+					"Err while getting the user from the checkuser route: ",
+					err
+				)
+			);
+	}
+
+	syncCurrentUSer(user) {
+		this.setState({ currentUser: user });
+	}
 	render() {
 		return (
 			<div>
 				<NavBar />
 				<Switch>
-					{/* <Route exact path="/" component={Home} /> */}
+					<Route exact path="/" component={Home} />
 					<Route exact path="/login" component={LogIn} />
-					<Route path="/signup" component={SignUp} />
-					<Route path="/timeline" component={Timeline} />
+					<Route exact path="/signup" component={SignUp} />
+					<Route exact path="/timeline" component={Timeline} />
 				</Switch>
 			</div>
 		);
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,36 @@
-import React from 'react'
-import './NavBar.css'
+import React from "react";
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
+import Home from "./Home";
 
+export default class NavBar extends React.Component {
+	render() {
+		return (
+			<div>
+				<div className="nav">
+					{/* <input type="checkbox" id="nav-check"/> */}
+					<div className="nav-header">
+						<div className="nav-title">
+							<div className="nav-brand">Contextual TimeLines</div>
+						</div>
+					</div>
+					<div className="nav-btn">
+						<label for="nav-check">
+							<span></span>
+							<span></span>
+							<span></span>
+						</label>
+					</div>
 
-export default class NavBar extends React.Component{
-    render(){
-        return(
-            <div>
-                <div className="nav">
-  {/* <input type="checkbox" id="nav-check"/> */}
-  <div className="nav-header">
-    <div className="nav-title">
-        <div className ='nav-brand'>
-      TimeLine
-     </div>
-    
-    </div>
-  </div>
-  <div className="nav-btn">
-    <label for="nav-check">
-      <span></span>
-      <span></span>
-      <span></span>
-    </label>
-  </div>
-  
-  <div className="nav-links">
-    <a href="#"  target="_blank">Home</a>
-    <a href="#"  target="_blank">LogIn</a>
-    <a href="#"  target="_blank">SignUp</a>
-    
-  </div>
-</div>
-
-
-
-            </div>
-        )
-    }
+					<div className="nav-links">
+						<Link to={"/"}>{Home}Home</Link>
+						<Link to={"/signup"}>{SignUp}Signup</Link>
+						<Link to={"/login"}>{LogIn}Login</Link>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
