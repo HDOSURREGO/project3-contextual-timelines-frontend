b17c0196ede4ef8851c83017beac90a5f619883e
Henry Hoyos <hdosurrego@gmail.com>
2019-11-23 08:12:32 -0500

    Timeline Compare Fixed

--- src/components/TimelineCompare.js
+++ src/components/TimelineCompare.js
@@ -7,7 +7,9 @@ export default class Timeline extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			timelineSelected: null,
+			timelineSelected1: null,
+			timelineSelected2: null,
+			timelineSelected3: null,
 			timelineName: "",
 			eventTitle: "",
 			eventDate: { type: Date },
@@ -15,7 +17,9 @@ export default class Timeline extends React.Component {
 			timelines: [],
 			events: []
 		};
-		this.showTimelineInfo = this.showTimelineInfo.bind(this);
+		this.showTimelineInfo1 = this.showTimelineInfo1.bind(this);
+		this.showTimelineInfo2 = this.showTimelineInfo2.bind(this);
+		this.showTimelineInfo3 = this.showTimelineInfo3.bind(this);
 	}
 
 	componentDidMount() {
@@ -36,14 +40,36 @@ export default class Timeline extends React.Component {
 			});
 	}
 
-	showTimelineInfo(event) {
+	showTimelineInfo1(event) {
 		console.log("Este es el id del timeline seleccionado:", event.target.value);
 		const selectedTimelines = this.state.timelines.filter(
 			chosenTimeline => chosenTimeline._id === event.target.value
 		);
 
 		this.setState({
-			timelineSelected: selectedTimelines[0]
+			timelineSelected1: selectedTimelines[0]
+		});
+	}
+
+	showTimelineInfo2(event) {
+		console.log("Este es el id del timeline seleccionado:", event.target.value);
+		const selectedTimelines = this.state.timelines.filter(
+			chosenTimeline => chosenTimeline._id === event.target.value
+		);
+
+		this.setState({
+			timelineSelected2: selectedTimelines[0]
+		});
+	}
+
+	showTimelineInfo3(event) {
+		console.log("Este es el id del timeline seleccionado:", event.target.value);
+		const selectedTimelines = this.state.timelines.filter(
+			chosenTimeline => chosenTimeline._id === event.target.value
+		);
+
+		this.setState({
+			timelineSelected3: selectedTimelines[0]
 		});
 	}
 
@@ -69,20 +95,20 @@ export default class Timeline extends React.Component {
 						<div className="timeline-header">
 							<select
 								className="select-button"
-								onChange={this.showTimelineInfo}
+								onChange={this.showTimelineInfo1}
 							>
 								{optionItems}
 							</select>
 
 							<h2 className="timeline-header__title">
-								{this.state.timelineSelected
-									? this.state.timelineSelected.timelineName
+								{this.state.timelineSelected1
+									? this.state.timelineSelected1.timelineName
 									: ""}
 							</h2>
 						</div>
 						<div className="timeline-container" id="timeline-1">
-							{this.state.timelineSelected
-								? this.state.timelineSelected.events.map(eachEvent => (
+							{this.state.timelineSelected1
+								? this.state.timelineSelected1.events.map(eachEvent => (
 										<div className="timeline-header">
 											<h3 className="timeline-header__subtitle">
 												{eachEvent.eventTitle}
@@ -111,20 +137,20 @@ export default class Timeline extends React.Component {
 						<div className="timeline-header">
 							<select
 								className="select-button"
-								onChange={this.showTimelineInfo}
+								onChange={this.showTimelineInfo2}
 							>
 								{optionItems}
 							</select>
 
 							<h2 className="timeline-header__title">
-								{this.state.timelineSelected
-									? this.state.timelineSelected.timelineName
+								{this.state.timelineSelected2
+									? this.state.timelineSelected2.timelineName
 									: ""}
 							</h2>
 						</div>
 						<div className="timeline-container" id="timeline-1">
-							{this.state.timelineSelected
-								? this.state.timelineSelected.events.map(eachEvent => (
+							{this.state.timelineSelected2
+								? this.state.timelineSelected2.events.map(eachEvent => (
 										<div className="timeline-header">
 											<h3 className="timeline-header__subtitle">
 												{eachEvent.eventTitle}
@@ -153,20 +179,20 @@ export default class Timeline extends React.Component {
 						<div className="timeline-header">
 							<select
 								className="select-button"
-								onChange={this.showTimelineInfo}
+								onChange={this.showTimelineInfo3}
 							>
 								{optionItems}
 							</select>
 
 							<h2 className="timeline-header__title">
-								{this.state.timelineSelected
-									? this.state.timelineSelected.timelineName
+								{this.state.timelineSelected3
+									? this.state.timelineSelected3.timelineName
 									: ""}
 							</h2>
 						</div>
 						<div className="timeline-container" id="timeline-1">
-							{this.state.timelineSelected
-								? this.state.timelineSelected.events.map(eachEvent => (
+							{this.state.timelineSelected3
+								? this.state.timelineSelected3.events.map(eachEvent => (
 										<div className="timeline-header">
 											<h3 className="timeline-header__subtitle">
 												{eachEvent.eventTitle}
